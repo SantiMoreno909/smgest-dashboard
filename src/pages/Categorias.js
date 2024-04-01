6457c5a2a41c71098072a0aef1e91a1470578ccd
Santi Moreno <santiagom.contact@gmail.com>
2024-03-31 21:44:07 -0300

    Modelo con totales, productos y usuarios funcionando

--- src/pages/Categorias.js
+++ src/pages/Categorias.js
@@ -1,7 +1,39 @@
+import React from "react";
+import { useProduct } from "../hooks/useProducts";
+import { Link } from "react-router-dom";
+
 const Categorias = () => {
+  // Utiliza el hook useProduct para obtener los datos de los Categorias
+  const { productos } = useProduct();
+
   return (
-    <div>
-      <p>Categorias</p>
+    <div className="sectionContainer">
+      <h2>Listado de Productos</h2>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Presentación</th>
+            <th scope="col">Categoría</th>
+            <th scope="col">Subcategoría</th>
+            <th scope="col">Proveedor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* Mapea los productos y renderiza cada uno en una fila de la tabla */}
+          {productos.map((producto) => (
+            <tr key={producto.id}>
+              <th scope="row">{producto.id}</th>
+              <td>{producto.name}</td>
+              <td>{producto.description}</td>
+              <td>{producto.category}</td>
+              <td>{producto.subcategory}</td>
+              <td>{producto.provider}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
